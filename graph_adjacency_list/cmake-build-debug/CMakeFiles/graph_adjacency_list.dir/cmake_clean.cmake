d7a4ec1600bebd12cd5fd99a3bb4d9eafed38063
wyhallenwu <wyhforwork66@gmail.com>
2021-04-01 23:32:38 +0800

    2021 4 01 update

---
+++ graph_adjacency_list/cmake-build-debug/CMakeFiles/graph_adjacency_list.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graph_adjacency_list.dir/main.cpp.obj"
+  "graph_adjacency_list.exe"
+  "graph_adjacency_list.exe.manifest"
+  "graph_adjacency_list.pdb"
+  "libgraph_adjacency_list.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graph_adjacency_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ graph_adjacency_list/main.cpp
@@ -0,0 +1,63 @@
+
+#include<iostream>
+#include <vector>
+using namespace std;
+
+template <typename T>
+struct node{
+    T data;
+    vector<T> relation;
+    node(){}
+    node(T data_){
+        data=data_;
+    }
+};
+//directed graph
+template <typename T>
+class graph_adjacency_list{
+protected:
+    node<T> *graph_list;
+    int size;
+    int max_size;
+public:
+    graph_adjacency_list(T *array,int size_,int max_size_){
+        size=size_;
+        max_size=max_size;
+        graph_list=new node<T> [max_size];
+        for(int i=0;i<size;i++)
+            graph_list[i].data=array[i];
+    }
+
+    virtual ~graph_adjacency_list(){
+        delete []graph_list;
+    }
+
+    int find_index(T data_){
+        for(int i=0;i<size;i++)
+            if(graph_list[i].data==data_)
+                return i;
+    }
+
+    void change_relation(T data1,T data2){
+        int index1=find_index(data1);
+        int index2=find_index(data2);
+        
+    }
+
+    void show(){
+    }
+
+};
+
+int main(){
+    int a[6]={1,2,3,4,5,6};
+    graph_adjacency_list<int> l(a,6,10);
+    l.change_relation(1,2);
+    l.change_relation(2,1);
+    l.change_relation(1,3);
+    l.change_relation(1,4);
+    l.change_relation(3,3);
+    l.change_relation(5,6);
+    l.change_relation(4,1);
+    l.show();
+}
