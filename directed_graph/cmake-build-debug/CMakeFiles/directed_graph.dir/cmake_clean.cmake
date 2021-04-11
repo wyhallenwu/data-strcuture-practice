083109008593eab881189d4180f0db909776c3b0
wyhallenwu <wyhforwork66@gmail.com>
2021-04-11 19:03:09 +0800

    2021 4 11 update

---
+++ directed_graph/cmake-build-debug/CMakeFiles/directed_graph.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/directed_graph.dir/main.cpp.obj"
+  "directed_graph.exe"
+  "directed_graph.exe.manifest"
+  "directed_graph.pdb"
+  "libdirected_graph.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/directed_graph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ directed_graph/cmake-build-debug/CMakeFiles/undirected_graph.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/undirected_graph.dir/main.cpp.obj"
+  "libundirected_graph.dll.a"
+  "undirected_graph.exe"
+  "undirected_graph.exe.manifest"
+  "undirected_graph.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/undirected_graph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
