8f407372e231a816591b7f83d0bc25fb21477fb3
wyhallenwu <wyhforwork66@gmail.com>
2021-04-18 19:59:01 +0800

    2021 4 18 update

--- graph_adjacency_list/main.cpp
+++ graph_adjacency_list/main.cpp
@@ -1,20 +1,8 @@
-
+//directed graph
 #include<iostream>
 #include <vector>
 #include <queue>
 using namespace std;
-
-struct edge{
-    int index1;
-    int index2;
-    double weighted;
-    edge(){}
-    edge(int index1_,int index2_,double weighted_){
-        index1=index1_;
-        index2=index2_;
-        weighted=weighted_;
-    }
-};
 
 template <typename T>
 struct triple_{
@@ -78,10 +66,8 @@ public:
     void change_relation(T data1,T data2,double weight_){
         int index1=find_index(data1);
         int index2=find_index(data2);
-        triple_<T> p2(index2, data2,weight_);
-        triple_<T> p1(index1, data1,weight_);
-        graph_list[index1].relation.push_back(p2);
-        graph_list[index2].relation.push_back(p1);
+        triple_<T> p(index2, data2,weight_);
+        graph_list[index1].relation.push_back(p);
     }
 
     void show(){
@@ -100,6 +86,8 @@ public:
         for(int i=0;i<size;i++)
             visited[i]=false;
         int index=find_index(data_);
+        cout<<graph_list[index].data<<"--";
+        visited[index]=true;
         DFS_help(index,visited);
     }
 
@@ -109,7 +97,7 @@ public:
             {
                 cout<<graph_list[i].relation[j].value<<"--";
                 visited[graph_list[i].relation[j].index]=true;
-                DFS_help(j,visited);
+                DFS_help(graph_list[i].relation[j].index,visited);
             }
         }
     }
@@ -137,11 +125,46 @@ public:
         }
     }
 
-
-    void Kruskal(){
-        vector<double> weighted;
-
+    void Dijkstra(T data_)  // directed graph
+    {
+        bool *sptSet = new bool[size];
+        for(int i=0;i<size;i++)
+            sptSet[i] = false;
+        double *distance=new double [size];
+        for(int i=0;i<size;i++)
+            distance[i]=INT_MAX;
+        int index=find_index(data_);
+        int count=0;
+        distance[index]=0;
+        while(count < size)
+        {
+            int loop_index = find_min_index_Dij(sptSet,distance);
+            for(int i=0;i<graph_list[loop_index].relation.size();i++){
+                int re_index = graph_list[loop_index].relation[i].index;
+                if(sptSet[re_index] == false) {
+                    double tmp = graph_list[loop_index].relation[i].weight + distance[loop_index];
+                    if (tmp < distance[re_index])
+                        distance[re_index] = tmp;
+                }
+            }
+            sptSet[loop_index] = true;
+            count++;
+        }
+        for(int i=0;i<size;i++)
+            cout<<distance[i]<<"--";
     }
+
+    int find_min_index_Dij(bool *&spt,double *&distance){
+        int min=INT_MAX,min_index;
+        for(int i=0;i<size;i++) {
+            if (spt[i] == false && distance[i] < min) {
+                min = distance[i];
+                min_index = i;
+            }
+        }
+        return min_index;
+    }
+
 
 };
 
@@ -151,13 +174,15 @@ int main(){
     l.change_relation(1,2,10.2);
     l.change_relation(1,3,4.5);
     l.change_relation(1,4,6.4);
-    l.change_relation(2,4,3.8);
+    l.change_relation(2,4,3.6);
     l.change_relation(4,5,14);
     l.change_relation(5,6,9.6);
     l.change_relation(6,1,7.2);
     l.show();
     cout<<"DFS"<<endl;
-    l.DFS(3);
+    l.DFS(6);
     cout<<endl<<"BFS"<<endl;
-    l.BFS(3);
+    l.BFS(4);
+    cout<<endl<<"Dij shortest path: "<<endl;
+    l.Dijkstra(1);
 }
